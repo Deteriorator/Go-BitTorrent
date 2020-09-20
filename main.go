2f1213b503f497cd13e8ffbb85190b08e5bcc027
Leon <Liangz.org@gmail.com>
2020-09-20 23:06:09 +0800

    feat: add main.go

---
+++ main.go
@@ -0,0 +1,46 @@
+/*
+ * @program: Go-BitTorrent
+ * @author: Leon
+ * @create: 2020-09-20 22:44
+ */
+package main
+
+import (
+	"github.com/jackpal/bencode-go"
+	"io"
+)
+
+type bencodeInfo struct {
+	Pieces      string `bencode:"pieces"`
+	PieceLength int    `bencode:"piece length"`
+	Length      int    `bencode:"length"`
+	Name        string `bencode:"name"`
+}
+
+type bencodeTorrent struct {
+	Announce string      `bencode:"announce"`
+	Info     bencodeInfo `bencode:"info"`
+}
+
+// Open parses a torrent file
+func Open(r io.Reader) (*bencodeTorrent, error) {
+	bto := bencodeTorrent{}
+	err := bencode.Unmarshal(r, &bto)
+	if err != nil {
+		return nil, err
+	}
+	return &bto, nil
+}
+
+type TorrentFile struct {
+	Announce    string
+	InfoHash    [20]byte
+	PieceHashes [][20]byte
+	PieceLength int
+	Length      int
+	Name        string
+}
+
+func (bto bencodeTorrent) toTorrentFile() (TorrentFile, error) {
+	// …
+}
